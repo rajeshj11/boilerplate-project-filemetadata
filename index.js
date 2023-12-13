f22229ae46974a472a5a14c6a4ba7fed808862f5
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2023-12-13 10:59:42 +0530

    hanlded the upload endpoint

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({storage})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +15,20 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const fileInfo = {
+    filename: file.originalname,
+    mimetype: file.mimetype,
+    size: file.size,
+  };
+
+  res.json(fileInfo);
+});
 
 
 const port = process.env.PORT || 3000;
